d0175654c46b505f38f52fcb73fe9f03e356500a
Haxintosh <92402488+Haxintosh@users.noreply.github.com>
2023-09-28 19:41:56 -0400

    Update findSP.py

--- findSP.py
+++ findSP.py
@@ -1,3 +1,5 @@
+# License: MIT
+
 def listDivisors(n):
     ans = []
     if n < 0:
@@ -5,28 +7,55 @@ def listDivisors(n):
     n = abs(int(n))
     ans = []
     for i in range(1, n+1):
-        if n%i == 0:
+        if n % i == 0:
             ans.append(i)
-
     return ans
+
 
 def findSP(mtn, mpn):
     divsList = listDivisors(mtn)
+    if mpn < 0:
+        divsList.append(-1)
+    mpn = abs(mpn)
     ans = []
     y = divsList[0]
     divsList[0] = 1
     for i in divsList:
-        mtnDi = round(mtn*y/i) # breaking every coding conventions known to mankind
+        mtnDi = round(mtn*y/i)  # breaking every coding conventions known to mankind
         if mtnDi + i == mpn:
             ans += [i, mtnDi]
-            break # out of the calculatoe
+            break  # out of the calculatoe
+    if divsList[-1] == -1:  # Jank to the max!
+        ans[0] *= -1
+        ans[1] *= -1
     return ans
 
 
-mtn = int(input("m*n: "))
-mpn = int(input("m+n: "))
-ans1 = findSP(mtn, mpn)
-if ans1 == []:
-    print("Oh noes not a single answer was found, what a shame really!")
-else:
+def verify(mtn, mpn, ans):
+    truth = False
+    if mpn == ans[0]+ans[1]:
+        truth = True
+    else:
+        truth = False
+
+    if mtn == ans[0]*ans[1]:
+        truth = True
+    else:
+        truth = False
+
+    return truth
+
+
+def main():
+    mtn = int(input("m*n: "))
+    mpn = int(input("m+n: "))
+    ans1 = findSP(mtn, mpn)
+    truth = verify(mtn, mpn, ans1)
     print(ans1)
+    if truth:
+        print("VERIFIED!")
+    else:
+        print("BAD!")
+
+
+main()
